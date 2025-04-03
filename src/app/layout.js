b80189e5545eca92bcc1641504ea74a534f58de9
Dev Makwana <devmakwana7704@gmail.com>
2025-04-03 16:45:54 +0530

    v0.0.3 - changing app name

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'CodeEnv - Python Coding Environment',
+  title: 'InterCode - Coding Environment',
   description: 'Online Python coding environment for interviews and practice',
 };
 
